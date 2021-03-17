9e3cc5bab4b7f72473e31c6bb2f65b856a951e7a
Mehrdod Odilzoda <odilzoda@gmail.com>
2021-03-17 10:58:50 +0500

    added some routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/mehrdod/todo"
+	"github.com/mehrdod/todo/pkg/handler"
+	"github.com/mehrdod/todo/pkg/repository"
+	"github.com/mehrdod/todo/pkg/service"
+	"log"
+)
 
+func main() {
+	srv := new(todo.Server)
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	if err := srv.Startup("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error during the server startup: %s", err.Error())
+	}
 }
--- server.go
+++ server.go
@@ -1,1 +1,26 @@
 package todo
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Startup(port string, handler http.Handler) error {
+	s.httpServer = &http.Server{
+		Addr:           ": " + port,
+		Handler:        handler,
+		MaxHeaderBytes: 1 << 20,
+		ReadTimeout:    20 * time.Second,
+		WriteTimeout:   40 * time.Second,
+	}
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) {
+	s.httpServer.Shutdown(ctx)
+}
